f83656d7d4eb9cfece4cd25e6c93392fd3c00f56
wahyunieka <ekaw0106@gmail.com>
2019-01-09 22:16:17 +0700

    update image contact-us index.php

---
+++ application/views/contact-us/index.php
@@ -0,0 +1,85 @@
+<?php $this->load->view('layouts/header.php') ?>
+	<!-- Hero section -->
+	<section class="hero-section set-bg" data-setbg="<?php echo base_url(); ?>assets/img/banner/1.jpg">
+		<div class="container">
+			<div class="hero-text text-white">
+				<p class="banner-title">Contact us &<br><span>Staff Only</span><br>School</p><br>
+				<p>on this page,you can read about all our activity and more</p>
+			</div>
+		</div>
+	</section>
+	<body>
+	<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3989.6398691574464!2d101.41372441410526!3d0.5419040637909304!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31d5ab9721474cdd%3A0xe0e1f307148bd748!2sJimboRee+Kinderland!5e0!3m2!1sid!2sid!4v1547022279500" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+	</body>
+	
+	
+<!-- categories section -->
+
+
+<section class="categories-section spad">
+		<div class="container">
+			<div class="section-title">
+				<p>Contact Info </p>
+			</div>
+			<div class="row">
+				<!-- categorie -->
+				<div class="col-lg-4">
+					<div class="categorie-item">
+						
+						<div class="ci-text">
+							<h5>Address</h5>
+							
+									<div class="row">
+										<div class="col-lg-2">
+											<span class="icon"><i class="fa fa-map-marker"></i></span>
+										</div>
+										<div class="col-lg-10">
+											<p class="footer-content">Jalan Pemuda, Pemuda City Walk, Block B (Education), No. 11, 12, 15, Tampan, Payung Sekaki, Tampan, Payung Sekaki, Pekanbaru City, Riau 28292</p>
+										</div>
+									</div>
+								
+							<h5>Phone</h5>
+							
+									<div class="row">
+										<div class="col-lg-2">
+											<span class="icon"><i class="fa fa-phone"></i></span>	
+										</div>
+										<div class="col-lg-10">
+											<p class="footer-contet">0821-6976-4578</p>
+										</div>
+									</div>
+								
+							<h5>Email</h5>
+							
+									<div class="row">
+										<div class="col-lg-2">
+											<span class="icon"><i class="fa fa-envelope-o"></i></span>
+										</div>
+										<div class="col-lg-10">
+											<p class="footer-content">jimboree_@gmail.com</p>
+										</div>
+									</div>
+				
+							<h5>Opening Hours</h5>
+							<P class="footer-content">Monday - Friday </P>
+							<p> 08.30 - 05.00 PM </p>
+							
+						</div>
+					</div>
+				</div>
+				<!-- categorie -->
+<!-- banner section -->
+<section class="section-title mb-0 pb-2">
+		
+				<p>We would Love To Hear From You!</p>
+				<p> If you have any questions,please call us or fill in the form below and we will get back to you very soon.</p>
+				<input type="submit" class="site-btn" value="Send Message">
+				<!-- <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec malesuada lorem maximus mauris scelerisque, at rutrum nulla dictum. Ut ac ligula sapien. Suspendisse cursus faucibus finibus.</p> -->
+			
+					
+				
+			
+	</section>
+	<!-- banner section end -->
+	
+<?php $this->load->view('layouts/footer.php') ?>
--- application/views/layouts/header.php
+++ application/views/layouts/header.php
@@ -17,7 +17,7 @@
 	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/style.css"/>
 	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/custom/main.css">
 	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/custom/homepage.css">
-
+	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/custom/aboutus.css">
 	<!--[if lt IE 9]>
 	  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 	  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
