297c711f59f7aa8bc3b2fb12f396bd201080417a
wahyunieka <ekaw0106@gmail.com>
2019-01-09 22:38:29 +0700

    update about-us.php

---
+++ application/views/h/about-us.php
@@ -0,0 +1,208 @@
+<?php $this->load->view('layouts/header.php') ?>
+	<!-- Hero section -->
+	<section class="hero-section set-bg" data-setbg="<?php echo base_url(); ?>assets/img/banner/1.jpg">
+		<div class="container">
+			<div class="hero-text text-white">
+			<h2>About us </h2>
+				<p>on this page,you can read about all our activity and more.</p>
+			</div>
+			</div>
+		</div>
+	</section>
+	<!-- categories section -->
+	<section class="categories-section spad">
+	<!-- categorie -->
+		<div class="col-lg-4">
+			<div class="categorie-item">
+				<div class="ci-text">
+					<p><h3>We are JimboRee based <br>Primary School focused<br>on excellence.</h3></p>
+						<p>Every child is a gem.in our primary school,we'll polish your gem and turn boy or girl into a real diamond. </p>
+						<p>no matter what scores you child have,we can help him or her to figure out life priorities and denote the path.All children are unique - this is our motto! We have the best teachers with a personal approach to each student.</p>
+							<div class="ci-thumb set-bg" data-setbg="<?php echo base_url(); ?>assets/img/section2/1.jpg"></div>
+								<div class="row">
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- categories section end -->
+	
+	<section class="search-section">
+		<div class="container">
+		<div class="right-title text-white">
+				<h10>Parents choise us</h10>
+					<h2>Why jimboRee?</h2>
+			<div class="row">
+				<div class="col-lg-3">
+					<div class="icon"><i class="fa fa-bullseye"></i></div>
+					<p class="search-caption">100%</p>
+					<p class="search-caption2">Passing to Universities</p>
+				</div>
+				<div class="col-lg-3">
+					<div class="icon"><i class="fa fa-users"></i></div>
+					<p class="search-caption">126</p>
+					<p class="search-caption2">People Working</p>
+				</div>
+				<div class="col-lg-3">
+					<div class="icon"><i class="fa fa-suitcase"></i></div>
+					<p class="search-caption">1231</p>
+					<p class="search-caption2">Student Enrolled</p>
+				</div>
+				<div class="col-lg-3">
+					<div class="icon"><i class="fa fa-user"></i></div>
+					<p class="search-caption">433</p>
+					<p class="search-caption2">Happy Smiles</p>
+					</div>
+				<div class="row">
+					<div class="col-md-10 offset-md-1">
+						<!-- search form -->
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- search section end -->
+	<!-- course section -->
+	<section class="course-section spad">
+		<div class="container">
+		<div class="col-lg-4">
+			<div class="categorie-item">
+			<div class="right-title mb-0">
+			<div class="ci-thumb set-bg" data-setbg="<?php echo base_url(); ?>assets/img/section2/1.jpg"></div>
+			
+				<h2>Our Mission</h2>
+				<p><h4> The primary school is the most important begining point.</h4></p>
+				<p> jimBoree Education exists to provide international education to our young students </p>
+				<p>through a commitment to English language Fluecy,Technological expertise and anquiry</p>
+				<p> basede teaching & amp;learning,we deliver an internationally recognized program that integrates global conceps wit respect for our Indonesian national culture and identity</p>
+				<p> (JimboRee education hadir dalam rangka memberikan pendidikan bersekala Internasional kepada pembelajar Muda ,melaluli komitmen kepada penggunaan Bahasa inggris secara lancar ,keterampilan teknologi ,pembelajaran ,berbasis tanya jawab, kami menyebarkan program yang telah dikenal secara internasional yang menyatukan konsep global yang menghormati budaya serta identitas Indonesia)</p>
+			</div>
+		</div>
+		</section>
+		
+		<section class="course-section spad">
+		<div class="container">
+		<div class="col-lg-4">
+			<div class="categorie-item">
+			<div class="ci-thumb set-bg" data-setbg="<?php echo base_url(); ?>assets/img/section2/1.jpg"></div>
+			
+		<h2>Our Vision</h2>
+				<p><h4> The primary education is for everyone</h4></p>
+				<p> To be leading international standard educational facility providing. high quality tutoring to</p>
+				<p>young students to excel through t5heir lives in rapidly changing world.</p>		
+				<p>(Menjadi panutan dalam pendidikan bersekala internasional yang memberikan fasilitas pengajaran berkualitas kepada pelajar muda 
+				sehingga mereka dapat memberi pengaruh yang merubah dunia)</p>
+			</div>
+		</div>
+	<!-- course section end -->
+
+	<!-- categories section -->
+	<section class="categories-section spad">
+		<div class="container">
+			<div class="section-title">
+				<p>News & Announcement. </p>
+			</div>
+			<div class="row">
+				<!-- categorie -->
+				<div class="col-lg-4">
+					<div class="categorie-item">
+						<div class="ci-thumb set-bg" data-setbg="http://localhost/jimboree-web/assets/img/announcements/1.png"></div>
+						<div class="ci-text">
+							<p>December 9,2018 </p>
+							<h5>New Schedule</h5>
+							<p>Dear parents,please,consider that our timetable will be changed in several months. </p>
+							
+						</div>
+					</div>
+				</div>
+				<!-- categorie -->
+				<div class="col-lg-4">
+					<div class="categorie-item">
+						<div class="ci-thumb set-bg" data-setbg="http://localhost/jimboree-web/assets/img/announcements/2.jpg"></div>
+						<div class="ci-text">
+						<p>December 9,2018</p>
+							<h5>Top Students This Month</h5>
+							<p> We would like to introduce the students who showed amazing progress for the last 30 days.</p>
+							
+						</div>
+					</div>
+				</div>
+				<!-- categorie -->
+				<div class="col-lg-4">
+					<div class="categorie-item">
+						<div class="ci-thumb set-bg" data-setbg="http://localhost/jimboree-web/assets/img/announcements/3.jpg"></div>
+						<div class="ci-text">
+							<p>December 9,2018</p>
+							<h5>Gap Analysis</h5>
+							<p>Are you sure your child do everything well? Read this article to find this out.</p>
+							
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- categories section end -->
+
+<!-- gallery section -->
+<section class="foto-section">
+		<div class="row">
+			<div class="col-lg-4">
+				<div class="categorie-item">
+					<div class="ci-thumb set-bg" data-setbg="http://localhost/jimboree-web/assets/img/foto/1.jpg"></div>
+				</div>
+			</div>
+			<div class="col-lg-4">
+				<div class="categorie-item">
+					<div class="ci-thumb set-bg" data-setbg="http://localhost/jimboree-web/assets/img/foto/2.jpg"></div>
+				</div>
+			</div>
+			<div class="col-lg-4">
+				<div class="categorie-item">
+					<div class="ci-thumb set-bg" data-setbg="http://localhost/jimboree-web/assets/img/foto/3.jpg"></div>
+				</div>
+			</div>
+			<div class="col-lg-4">
+				<div class="categorie-item">
+					<div class="ci-thumb set-bg" data-setbg="http://localhost/jimboree-web/assets/img/foto/4.png"></div>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-lg-4">
+				<div class="categorie-item">
+					<div class="ci-thumb set-bg" data-setbg="http://localhost/jimboree-web/assets/img/foto/5.jpg"></div>
+						</div>
+							</div>
+				<div class="col-lg-4">
+					<div class="categorie-item">
+						<div class="ci-thumb set-bg" data-setbg="http://localhost/jimboree-web/assets/img/foto/6.jpg"></div>
+				</div>
+			</div>
+	</section>
+	<!-- gallery section end -->
+
+	<!-- categorie -->
+	<!-- gallery section -->
+	<div class="col-lg-4">
+					<div class="categorie-item">
+						
+							<h5>School & Improvment</h5>
+							<p>Lorem ipsum dolor sit amet,ex falli congue postea pri,id per rebum congue iudico,regione mentitum adipiscing up pri.Luptatum corrumpit disputando 
+							sit et.Quit ut meis nihil legere.Eu vix cibo tritani,dicant patrioque interesset te vim.Ne ludus vitae electram per,vim cu option veritus blandit. Ex delectus deseruisse nam,his ex erat debitis omittantur.Ne erros dissentiet qui. </p>
+							<p>his partiendo deseruisse instructior et.Commune principes ullamcorper sea in,ea detraco consulatu </p>
+							<div class="ci-thumb set-bg" data-setbg="http://localhost/jimboree-web/assets/img/announcements/3.jpg"></div>
+						<div class="ci-text">
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- gallery section end -->
+<?php $this->load->view('layouts/footer.php') ?>
