0f3617521390d0c26c6b4b6ec38d6d0dadcb100c
dmrifqi <dimasrifqi296@gmail.com>
2019-01-23 21:08:33 +0700

    update students

--- application/views/contact-us/index.php
+++ application/views/contact-us/index.php
@@ -1,86 +1,104 @@
 <?php $this->load->view('layouts/header.php') ?>
 	<!-- Hero section -->
-	<section class="hero-section set-bg" data-setbg="<?php echo base_url(); ?>assets/img/banner/1.jpg">
+	<section class="hero-section set-bg" data-setbg="<?php echo base_url(); ?>assets/img/academic/banner/1.jpg">
 		<div class="container">
 			<div class="hero-text text-white">
 				<p class="banner-title">Contact us &<br><span>Staff Only</span><br>School</p><br>
-				<p>on this page,you can read about all our activity and more</p>
+				<p class="banner-subtitle">On this page, you can read about all our activity and more.</p>
 			</div>
+			<!-- <div class="row">
+				<div class="col-lg-10 offset-lg-1">
+					<form class="intro-newslatter">
+						<input type="text" placeholder="Name">
+						<input type="text" class="last-s" placeholder="E-mail">
+						<button class="site-btn">Sign Up Now</button>
+					</form>
+				</div>
+			</div> -->
 		</div>
 	</section>
-	<body>
-	
-	<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3989.6398691574464!2d101.41372441410526!3d0.5419040637909304!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31d5ab9721474cdd%3A0xe0e1f307148bd748!2sJimboRee+Kinderland!5e0!3m2!1sid!2sid!4v1547022279500" position=" absolute" width="1433" height="543" frameborder="1" style="border:1" allowfullscreen></iframe>
-	</body>
-	
-	
-<!-- categories section -->
+	<!-- Hero section end -->
 
 
-<section class="categories-section spad">
-		<div class="container">
-			<div class="section-title">
-				<p>Contact Info </p>
+	<!-- peta section -->
+		<section class="peta-section spad">
+			<div class="container">
+				<div class="col-lg-12">
+					<body>
+						<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3989.6398691574464!2d101.41372441410526!3d0.5419040637909304!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31d5ab9721474cdd%3A0xe0e1f307148bd748!2sJimboRee+Kinderland!5e0!3m2!1sid!2sid!4v1547022279500" position=" center" width="1200" height="543" frameborder="1" style="border:1" allowfullscreen></iframe>
+				</div>
 			</div>
-			<div class="row">
-				<!-- categorie -->
-				<div class="col-lg-4">
-					<div class="categorie-item">
+		</body>
+	</section>
+	<!-- peta section end -->
+
+	<!-- contactinfo -->	
+		
+		<div class="col-lg-12">
+			<div class="widget-item">
+				<p class="contact-info-title">Contact Info</p>
+					<div class="row">
+						<div class="col-lg-12">
+								<span class="icon"><i class="fa fa-map-marker"></i></span>
+									<p class="Address-opacity-title">Address<br></p>
+							        <p class="Address-opacity-caption">Jalan Pemuda, Pemuda City Walk, Block B (Education), No. 11, 12, 15, Tampan, Payung Sekaki, Tampan, Payung Sekaki, Pekanbaru City, Riau 28292</p>
+								</div>	
+						     </div>
+						</div>
+					
 						
-						<div class="ci-text">
-							<h5>Address</h5>
-							
-									<div class="row">
-										<div class="col-lg-2">
-											<span class="icon"><i class="fa fa-map-marker"></i></span>
-										</div>
-										<div class="col-lg-10">
-											<p class="footer-content">Jalan Pemuda, Pemuda City Walk, Block B (Education), No. 11, 12, 15, Tampan, Payung Sekaki, Tampan, Payung Sekaki, Pekanbaru City, Riau 28292</p>
-										</div>
-									</div>
-								
-							<h5>Phone</h5>
-							
-									<div class="row">
-										<div class="col-lg-2">
-											<span class="icon"><i class="fa fa-phone"></i></span>	
-										</div>
-										<div class="col-lg-10">
-											<p class="footer-contet">0821-6976-4578</p>
-										</div>
-									</div>
-								
-							<h5>Email</h5>
-							
-									<div class="row">
-										<div class="col-lg-2">
-											<span class="icon"><i class="fa fa-envelope-o"></i></span>
-										</div>
-										<div class="col-lg-10">
-											<p class="footer-content">jimboree_@gmail.com</p>
-										</div>
-									</div>
-				
-							<h5>Opening Hours</h5>
-							<P class="footer-content">Monday - Friday </P>
-							<p> 08.30 - 05.00 PM </p>
-							
+							<span class="icon"><i class="fa fa-phone"></i></span>	
+								<p class="Phone-title">Phone<br></p>
+							 	<p class="Phone-caption">0821-6976-4578</p>
+							</div>	
+						 </div>
+					</div>
+					
+						<span class="icon"><i class="fa fa-envelope-o"></i></span>
+							<p class="Email-title">Email<br></p>	
+							<p class="Email-caption">jimboree_@gmail.com</p>
 						</div>
 					</div>
-				</div>
-				<!-- categorie -->
-<!-- banner section -->
-<section class="section-title mb-0 pb-2">
-		
-				<p>We would Love To Hear From You!</p>
-				<p> If you have any questions,please call us or fill in the form below and we will get back to you very soon.</p>
-				<input type="submit" class="site-btn" value="Send Message">
-				<!-- <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec malesuada lorem maximus mauris scelerisque, at rutrum nulla dictum. Ut ac ligula sapien. Suspendisse cursus faucibus finibus.</p> -->
-			
+                                    
 					
-				
-			
+						<span class="icon"><i class="fa fa-book"></i></span>
+						<p class="opening-title">Opening Hours<br></p>
+						<p class="opening-caption">Monday - Friday 08.00 AM - 05.00 PM</p>
+					</div>
+				</div>
+					
+						<div class="col-lg-6">
+						<div class="categorie-item">
+							<p class="we-title">We would Love To Hear From You!<br></p>
+							<p class="we-caption"> If you have any questions,please call us or fill in the form below and we will get back to you very soon</p>
+						</div>
+					</div>
+							<form>
+							<div class="form-group">
+   							 <label for="emailAdress"></label>
+   								 <input type="text" class="form-control" id="emailAdress" placeholder="Your Name*">
+ 								 </div>
+ 							 <div class="form-group">
+ 							   <label for="exampleInputPassword1"></label>
+ 							   <input type="password" class="form-control" id="exampleInputPassword1" placeholder="E-mail*">
+ 								 </div>
+								  <div class="form-group">
+ 								   <label for="exampleInputPassword1"></label>
+ 								   <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Subject">
+ 									 </div>
+ 									 <div class="form-group">
+ 								   <label for="exampleInputPassword1"></label>
+ 									   <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Message">
+ 											 </div>
+  
+  										<button type="submit" class="btn btn-primary">Send Message</button>
+													</form>
+							
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- contactinfo end -->
 	</section>
-	<!-- banner section end -->
-	
+	</section>
 <?php $this->load->view('layouts/footer.php') ?>
--- application/views/students/index.php
+++ application/views/students/index.php
@@ -26,7 +26,7 @@
             </div>
             <div class="row">
 				<!-- categorie -->
-				<div class="col-lg-4">
+				<div class="col-lg-6">
 					<div class="categorie-item">
 						<div class="ci-thumb set-bg" data-setbg="<?php echo base_url(); ?>assets/img/students/4.png"></div>
 						<div class="ci-text">
@@ -36,17 +36,21 @@
 					</div>
                 </div>
                 <!-- categorie -->
-				<div class="col-lg-4">
+				
+				<div class="col-lg-6">
 					<div class="categorie-item">
 						<div class="ci-thumb set-bg" data-setbg="<?php echo base_url(); ?>assets/img/homepage/section2/2.jpg"></div>
 						<div class="ci-text">
-                            <h5>@jimboreeschool</h5>
+						<a href="https://www.facebook.com/Eka.wahyuni.130998" target="tampil">jimboree School</a>
+                            
 							<p>Lorem ipsum dolor sit amet, periculis expetendis usu no, choro commodo docendi ad sea. Et facilisis argumentum elaboraret sit.</p>
-						</div>
-					</div>
-                </div>
-                <!-- categorie -->
-				<div class="col-lg-4">
+						
+					<div class="categorie-item">
+						<div class="ci-thumb set-bg" data-setbg="<?php echo base_url(); ?>assets/img/homepage/section2/2.jpg"></div>
+						<div class="ci-text">
+							<h5>@jimboreeschool_ina</h5>
+							<p>Lorem ipsum dolor sit amet, periculis expetendis usu no, choro commodo docendi ad sea. Et facilisis argumentum elaboraret sit.</p>
+						
 					<div class="categorie-item">
 						<div class="ci-thumb set-bg" data-setbg="<?php echo base_url(); ?>assets/img/homepage/section2/2.jpg"></div>
 						<div class="ci-text">
@@ -55,7 +59,9 @@
 						</div>
 					</div>
 				</div>
+
 				</section>
+				<!-- Students Links end -->
 
 
 	<!-- Food Menu -->
@@ -199,32 +205,27 @@
 	<!-- Field Trip section -->
 	<section class="categories-section spad">
 		<div class="container">
-			<div class="section-title">
-				<p><h3>Field Trip</h3></p>
+			<div class="section-title"><p><h3>Field Trip</h3></p>
             	</div>
             		<div class="row">
 				<!-- categorie -->
-				<div class="col-lg-4">
+				<div class="col-lg-6">
 					<div class="categorie-item">
 						<div class="ci-thumb set-bg" data-setbg="<?php echo base_url(); ?>assets/img/trip/1.png"></div>
 						<div class="ci-text">
-							<p>December 9, 2018</p>
-							<p><h3>Lorem Ipsum</h3></p>
+						<p class="categorie-title">December 9, 2018</p>
+						<span class="categorie-title-caption">Lorem Ipsum</span>
 							<p>Lorem ipsum dolor sit amet, periculis expetendis usu no, choro commodo docendi ad sea. Et facilisis argumentum elaboraret sit, alia mucius putant quo cu, ex saperet labores ius. Cu nam putant virtute, duo malis labores omittam at. Eu quo fastidii mediocritatem, eos erant bonorum nominati at, sed at summo perfecto. Alienum lobortis indoctum ad mel, justo tation evertitur ex nam, vix quodsi recteque posidonium an. Ut accusata reformidans est. Clita quaerendum pri ad, cetero torquatos qui ei. Nusquam detracto oportere eu eam. In pro movet nullam voluptua, duo at facete molestiae prodesset. An ius quidam suscipit definitionem. An sit ornatus facilisi. Stet viris mea ne, errem altera accusata his id.</p>
 						</div>
 					</div>
                 </div>
                 <!-- categorie -->
-				<div class="col-lg-4">
+				<div class="col-lg-6">
 					<div class="categorie-item">
 						<div class="ci-thumb set-bg" data-setbg="<?php echo base_url(); ?>assets/img/trip/2.jpg"></div>
 						<div class="ci-text">
 							<p> Lorem ipsum dolor sit amet, periculis expetendis usu no, choro commodo docendi ad sea. Et facilisis argumentum elaboraret sit, alia mucius putant quo cu, ex saperet labores ius. Cu nam putant virtute, duo malis labores omittam at.</p>
-						</div>
-					</div>
-                </div>
-                <!-- categorie -->
-				<div class="col-lg-4">
+						
 					<div class="categorie-item">
 						<div class="ci-thumb set-bg" data-setbg="<?php echo base_url(); ?>assets/img/trip/3.jpg"></div>
 						<div class="ci-text">
