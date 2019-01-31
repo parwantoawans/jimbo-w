d65f54909bc050e8b936353dd6a55a3626608768
parwantoawans <parwanto@digitalbuana.com>
2019-01-31 21:24:07 +0700

    fixing video on aboutus page

--- application/views/about-us/index.php
+++ application/views/about-us/index.php
@@ -193,16 +193,17 @@
 		<div class="row">
 			<div class="col-lg-6">
 				<div class="categorie-item">
-				<video width="700" height="570" controls>
-  				<source src="1.mp4" type="vidio/mp4">
+						<video width="100%" height="100%" controls>
+  							<source src="assets/videos/1.mp4" type="video/mp4">
 						</video>
 				</div>
 			</div>
 			<div class="col-lg-6">
 				<div class="categorie-item">
-				<video width="655" height="570" controls>
-  				<source src="2.mp4" type="vidio/mp4">
-						</video>
+					<video width="100%" height="100%" controls>
+  						<source src="assets/videos/2.mp4" type="video/mp4">
+					</video>
+				</div>
 			</div>
 		</div>
 	</section>
